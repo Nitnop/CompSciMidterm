84ed0f9021c0fa0777badc2b988de1b4d2db17b7
nitnop <covepontin@gmail.com>
2023-03-23 12:00:34 -0600

    saving the inital skeleton

---
+++ ComputerBuilder.java
@@ -0,0 +1,9 @@
+public class ComputerBuilder {
+    // o shit here we go again meme
+
+    void getType(){
+        System.out.println("Welcome to Cove's midterm project that he's also" + 
+        "gonna use to teach a class with. Would you Like to build" + 
+        " a (L)aptop or a (D)esktop?");
+    }
+}
---
+++ ComputerBuilderDriver.java
@@ -0,0 +1,41 @@
+/*
+ * this is the java version of a call center
+ */
+
+
+
+public class ComputerBuilderDriver{
+
+    public static void main(String[] args) {
+        
+    // initialaize a bunch of garbage    
+
+    // to do: prompt the user weather they want to bulid a PC or a Laptop
+
+    // to do: make a new object to store all this info in, call it name.object or something
+
+    // to do: prompt the user for a name for this build
+
+    // to do: prompt the user for brand
+
+    // to do: prompt the user for case type
+
+    // to do: prompt the user for CPU brand
+
+    // to do: prompt the user for CPU speed
+
+    // to do: prompt the user for Hard Drive capacity
+
+    // to do: prompt user for monitor brand
+
+    // to do: prompt user for monitor size
+
+    // to do: make up a bunch of prices and options for all the above
+    // probably go to newegg.com and just use what I find there
+    
+    // to do: call a printf that outputs everthing they want and the prices
+    // and do it in a not ugly way
+    // and have a total at the bottom
+    }
+
+}
